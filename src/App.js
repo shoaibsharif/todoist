0814460d00ee9c80d668f5dee208d15cd8952880
Enamul Haque <enamul.iu91@gmail.com>
2019-10-18 12:44:34 +0600

    gulp file updated and sass removed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/layout/Header';
 import Content from './components/layout/Content';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className='App'>
       <Header />
       <Content />
+      <Footer />
     </div>
   );
 }
